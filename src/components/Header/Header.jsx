46ed9df10f4659338c4c6638dcb533bf9bf5c87e
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2026-05-07 14:12:54 +0530

    Improve header section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,34 @@
-import React from "react";
 import "./Header.css";
 
 const Header = ({ setCategory }) => {
+  // Handle menu scroll
+  const handleViewMenu = () => {
+    setCategory("All");
+
+    const exploreMenu = document.getElementById("explore-menu");
+
+    if (exploreMenu) {
+      exploreMenu.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+  };
+
   return (
     <header className="header">
       <section className="header-contents">
-
         <h1>Order your favorite food here</h1>
+
         <p>
           Choose from a diverse menu featuring a delectable array of dishes
           crafted with the finest ingredients and culinary expertise.
         </p>
+
         <button
-          onClick={() => {
-            setCategory("All");
-            document
-              .getElementById("explore-menu")
-              .scrollIntoView({ behavior: "smooth" });
-          }}
+          type="button"
+          className="view-menu-btn"
+          onClick={handleViewMenu}
         >
           View Menu
         </button>
