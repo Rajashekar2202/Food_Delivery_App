eda1e09961d17d5e5927b1cb4b883b221075456d
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2026-05-07 14:50:20 +0530

    fooddisplay changes

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -6,27 +6,34 @@ import FoodItem from "../FoodItem/FoodItem";
 const FoodDisplay = ({ category }) => {
   const { food_list } = useContext(StoreContext);
 
+  // Filter items
+  const filteredFoods =
+    category === "All"
+      ? food_list
+      : food_list.filter((item) => item.category === category);
+
   return (
-    <div className="food-display" id="food-display">
+    <section className="food-display" id="food-display">
       <h2>Top dishes near you</h2>
-      <div className="food-display-list">
-        {food_list.map((item) => {
-          if (category === "All" || category === item.category) {
-            return (
-              <FoodItem
-                key={item._id}
-                id={item._id}
-                name={item.name}
-                description={item.description}
-                price={item.price}
-                image={item.image}
-              />
-            );
-          }
-          return null;
-        })}
-      </div>
-    </div>
+
+      {/* EMPTY STATE */}
+      {filteredFoods.length === 0 ? (
+        <p className="no-food-text">No food items available.</p>
+      ) : (
+        <div className="food-display-list">
+          {filteredFoods.map((item) => (
+            <FoodItem
+              key={item._id}
+              id={item._id}
+              name={item.name}
+              description={item.description}
+              price={item.price}
+              image={item.image}
+            />
+          ))}
+        </div>
+      )}
+    </section>
   );
 };
 
