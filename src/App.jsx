5b85373894f3eb78f451e66310ec33479092655d
Rajashekar2202 <vemula.rajashekar02@gmail.com>
2026-05-07 15:37:12 +0530

    app is updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,56 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
+
 import { Route, Routes } from "react-router-dom";
 
 import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import LoginPopup from "./components/LoginPopup/LoginPopup";
+
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import PlaceOrder from "./pages/PlaceOrder";
-import Footer from "./components/Footer/Footer";
-import LoginPopup from "./components/LoginPopup/LoginPopup";
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
 
+  /* OPEN LOGIN */
+  const openLoginPopup = useCallback(() => {
+    setShowLogin(true);
+  }, []);
+
+  /* CLOSE LOGIN */
+  const closeLoginPopup = useCallback(() => {
+    setShowLogin(false);
+  }, []);
+
   return (
     <>
-      {showLogin && <LoginPopup setShowLogin={setShowLogin} />}
+      {/* LOGIN MODAL */}
+      {showLogin && (
+        <LoginPopup setShowLogin={setShowLogin} onClose={closeLoginPopup} />
+      )}
 
+      {/* APP LAYOUT */}
       <div className="app">
-        <Navbar setShowLogin={setShowLogin} />
-        <main>
+        {/* NAVBAR */}
+        <Navbar setShowLogin={openLoginPopup} />
+
+        {/* PAGE CONTENT */}
+        <main className="main-content">
           <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/order" element={<PlaceOrder />} />
-        </Routes>
+            <Route path="/" element={<Home />} />
+
+            <Route path="/cart" element={<Cart />} />
+
+            <Route path="/order" element={<PlaceOrder />} />
+          </Routes>
         </main>
       </div>
 
+      {/* FOOTER */}
       <Footer />
     </>
   );
 };
+
 export default App;
